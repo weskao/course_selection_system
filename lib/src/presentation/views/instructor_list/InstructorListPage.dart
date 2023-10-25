e71eba4340c261b97733611c2c023bbd461af3d8
Wes <gkm85663@gmail.com>
2023-10-26 04:17:37 +0800

    Switch + and - icon when press trailing button

--- lib/src/presentation/views/instructor_list/InstructorListPage.dart
+++ lib/src/presentation/views/instructor_list/InstructorListPage.dart
@@ -30,7 +30,7 @@ class InstructorListPage extends StatelessWidget {
         ),
         itemPadding: EdgeInsets.all(10.r),
         trailing: const Icon(Icons.add),
-        // trailing: const Icon(Icons.minimize),
+        trailingExpanded: const Icon(Icons.minimize),
         children: [
           ToggleListItem(
             title: Row(
