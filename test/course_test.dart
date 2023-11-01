a639d7c0c06fbdc632f62448ee5fe7949a15d693
Wes <weskao@fiami.com.tw>
2023-11-01 17:13:22 +0800

    Pass test case should return course list when instructorId exist

--- test/course_test.dart
+++ test/course_test.dart
@@ -13,50 +13,11 @@ void main() {
     apiService = CourseApiService(dataSource);
   });
 
-  group('getCourseList', () {
-    test('should return all courses when instructorId is null', () {
-      final result = apiService.getCourseList();
+  group('getCoursesByInstructorId', () {
+    test('should return course list when instructorId exist', () {
+      const existedInstructorId = 5;
+      final result = apiService.getCoursesByInstructorId(existedInstructorId);
       final List<Course> allCourseList = [
-        Course(
-          id: 1,
-          name: '基礎程式設計',
-          description: "基礎程式設計課程",
-          dayOfWeek: 2,
-          startTime: Time(hour: 10, minute: 0),
-          endTime: Time(hour: 12, minute: 0),
-        ),
-        Course(
-          id: 2,
-          name: '人工智慧總整與實作',
-          description: "人工智慧總整與實作課程",
-          dayOfWeek: 4,
-          startTime: Time(hour: 14, minute: 0),
-          endTime: Time(hour: 16, minute: 0),
-        ),
-        Course(
-          id: 3,
-          name: '訊號與系統',
-          description: "訊號與系統課程",
-          dayOfWeek: 5,
-          startTime: Time(hour: 10, minute: 0),
-          endTime: Time(hour: 12, minute: 0),
-        ),
-        Course(
-          id: 4,
-          name: 'C++實習',
-          description: "C++實習課程",
-          dayOfWeek: 3,
-          startTime: Time(hour: 19, minute: 0),
-          endTime: Time(hour: 21, minute: 0),
-        ),
-        Course(
-          id: 5,
-          name: '作業系統',
-          description: "作業系統課程",
-          dayOfWeek: 3,
-          startTime: Time(hour: 8, minute: 0),
-          endTime: Time(hour: 10, minute: 0),
-        ),
         Course(
           id: 6,
           name: '進階英文',
@@ -64,6 +25,7 @@ void main() {
           dayOfWeek: 3,
           startTime: Time(hour: 15, minute: 30),
           endTime: Time(hour: 18, minute: 30),
+          instructorId: 5,
         ),
         Course(
           id: 7,
@@ -72,6 +34,7 @@ void main() {
           dayOfWeek: 2,
           startTime: Time(hour: 9, minute: 0),
           endTime: Time(hour: 12, minute: 0),
+          instructorId: 113,
         ),
         Course(
           id: 8,
@@ -80,51 +43,24 @@ void main() {
           dayOfWeek: 4,
           startTime: Time(hour: 9, minute: 30),
           endTime: Time(hour: 12, minute: 30),
+          instructorId: 113,
         ),
+      ];
+
+      final expectedInstructorCourseList = [
         Course(
-          id: 9,
-          name: '動畫設計',
-          description: "動畫設計課程",
-          dayOfWeek: 1,
-          startTime: Time(hour: 16, minute: 0),
-          endTime: Time(hour: 18, minute: 0),
-        ),
-        Course(
-          id: 10,
-          name: '演算法',
-          description: "演算法課程",
+          id: 6,
+          name: '進階英文',
+          description: "進階英文課程",
           dayOfWeek: 3,
-          startTime: Time(hour: 11, minute: 0),
-          endTime: Time(hour: 12, minute: 0),
-        ),
-        Course(
-          id: 11,
-          name: '進階程式設計',
-          description: "進階程式設計課程",
-          dayOfWeek: 1,
-          startTime: Time(hour: 8, minute: 30),
-          endTime: Time(hour: 13, minute: 30),
-        ),
-        Course(
-          id: 12,
-          name: 'C++實習',
-          description: "C++實習課程",
-          dayOfWeek: 4,
-          startTime: Time(hour: 14, minute: 0),
-          endTime: Time(hour: 16, minute: 0),
-        ),
-        Course(
-          id: 13,
-          name: '組合語言',
-          description: "組合語言課程",
-          dayOfWeek: 2,
-          startTime: Time(hour: 15, minute: 0),
-          endTime: Time(hour: 17, minute: 0),
+          startTime: Time(hour: 15, minute: 30),
+          endTime: Time(hour: 18, minute: 30),
+          instructorId: 5,
         ),
       ];
 
       expect(result.code, 200);
-      expect(result.data.isEqualTo(allCourseList), isTrue);
+      expect(result.data.isEqualTo(expectedInstructorCourseList), isTrue);
     });
   });
 }
