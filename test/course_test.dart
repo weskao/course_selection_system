305645282837f8bbd3d260cd600cee039a3827f1
Wes <weskao@fiami.com.tw>
2023-11-01 17:40:28 +0800

    Pass "should return code 404 when instructorId not exist"

--- test/course_test.dart
+++ test/course_test.dart
@@ -91,5 +91,74 @@ void main() {
       expect(result.code, 200);
       expect(result.data.isEqualTo(expectedInstructorCourseList), isTrue);
     });
+
+    test('should return code 404 when instructorId not exist', () {
+      final allCourseList = [
+        Course(
+          id: 6,
+          name: '進階英文',
+          description: "進階英文課程",
+          dayOfWeek: 3,
+          startTime: Time(hour: 15, minute: 30),
+          endTime: Time(hour: 18, minute: 30),
+          instructorId: 5,
+        ),
+        Course(
+          id: 1,
+          name: '基礎程式設計',
+          description: "基礎程式設計課程",
+          dayOfWeek: 2,
+          startTime: Time(hour: 10, minute: 0),
+          endTime: Time(hour: 12, minute: 0),
+          instructorId: 7733,
+        ),
+        Course(
+          id: 2,
+          name: '人工智慧總整與實作',
+          description: "人工智慧總整與實作課程",
+          dayOfWeek: 4,
+          startTime: Time(hour: 14, minute: 0),
+          endTime: Time(hour: 16, minute: 0),
+          instructorId: 7733,
+        ),
+        Course(
+          id: 3,
+          name: '訊號與系統',
+          description: "訊號與系統課程",
+          dayOfWeek: 5,
+          startTime: Time(hour: 10, minute: 0),
+          endTime: Time(hour: 12, minute: 0),
+          instructorId: 7733,
+        ),
+      ];
+      final instructorList = [
+        Instructor(
+          id: 234,
+          name: "Floyd Miles",
+          rankLevel: 2,
+          avatarUrl: 'assets/images/default_avatar/default_avatar_5.jpg',
+          courseList: MockCourse.courseList2,
+        ),
+        Instructor(
+          id: 5,
+          name: "Savannah Nguyen",
+          rankLevel: 3,
+          avatarUrl: 'assets/images/default_avatar/default_avatar_8.jpg',
+          courseList: MockCourse.courseList3,
+        ),
+      ];
+
+      courseDataSource = CourseDataSource(allCourseList: allCourseList, instructorList: instructorList);
+      apiService = CourseApiService(courseDataSource);
+
+      const notExistedInstructorId = 0;
+      courseDataSource = CourseDataSource(allCourseList: allCourseList, instructorList: instructorList);
+      final result = apiService.getCoursesByInstructorId(notExistedInstructorId);
+
+      final List<Course> expectedInstructorCourseList = [];
+
+      expect(result.code, 404);
+      expect(result.data.isEqualTo(expectedInstructorCourseList), isTrue);
+    });
   });
 }
